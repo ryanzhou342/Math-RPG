583d94f206361de9a050de2545c75b39c1f2037e
Toben <sunsofmidngiht@gmail.com>
2020-02-08 15:58:32 -0400

    added a couple empty functions and comments

--- main/js/mathGames.js
+++ main/js/mathGames.js
@@ -1,1 +1,31 @@
 console.log("hello world");
+
+var playerHealth = 1000;
+var enemyHealth = 1000;
+
+
+
+function monster1stats(){
+    //establish stats for the first monster
+}
+
+function rndEquasion(){
+    //random equasion
+    //stored answer here
+}
+
+function rndSolution(){
+    //random answer by enemy
+}
+
+function attack(){
+    //player solve random equasion
+    //success = do damage
+}
+
+function defend(){
+    //player finds errors in enemie's solution
+    //every error found lowers received damage
+    // error located by ligne
+    //bad answers increase damage
+}
