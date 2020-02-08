bc2936a916562fa133c5ac5c86b977d258ac49ec
Ryan Zhou <ryanzhou342@gmail.com>
2020-02-08 16:50:30 -0400

    Finished generating random equation function

--- main/js/mathGames.js
+++ main/js/mathGames.js
@@ -3,15 +3,45 @@ console.log("hello world");
 var playerHealth = 1000;
 var enemyHealth = 1000;
 
-
+generate_equation();
 
 function monster1stats(){
     //establish stats for the first monster
 }
 
-function rndEquasion(){
-    //random equasion
-    //stored answer here
+function generate_equation() {
+    // Picks random numbers for the equation
+    var num1 = Math.floor(Math.random() * 100);
+    var num2 = Math.floor(Math.random() * 100);
+    
+    // Array for different types of equations
+    var equations = ["add", "subtract", "multiply", "divide"];
+    var equation_type = equations[Math.floor(Math.random() * equations.length)];
+	
+    // Generates an addition question
+    if (equation_type == "add") {
+	var answer = num1 + num2;
+	var equation = num1 + "+" + num2;
+    } else if (equation_type == "subtract") {
+	var answer = num1 - num2;
+	var equation = num1 + "-" + num2;
+    } else if (equation_type == "multiply") {
+	var answer = Math.floor(num1 / 10) * Math.floor(num2 / 10);
+	var equation = num1 + "*" + num2;
+    } else {
+	product = Math.floor(num1 / 10) * Math.floor(num2 / 10);
+	var answer = product / num1;
+	var equation = product + "/" + num1
+    }
+    
+    console.log("num1: " + num1);
+    console.log("num2: " + num2);
+    console.log(equation_type);
+    console.log(answer);
+    console.log("equation: " + equation);
+
+    // Returns an answer
+    return answer
 }
 
 function rndSolution(){
